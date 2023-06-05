2c4f82d969c72f58bedfc5c74439a620e99e9700
Johnny <johnny.darwin@gmail.com>
2023-06-05 12:09:36 +0200

    updated instance type to t3.micro for getting 2 vcpu with 1 GB memory

--- autoscaling-group.tf
+++ autoscaling-group.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "ecs_launch_config" {
     iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
     security_groups      = [aws_security_group.ecs_sg.id]
     user_data            = "#!/bin/bash\necho ECS_CLUSTER=RenewableCarbon-ecs-cluster >> /etc/ecs/ecs.config"
-    instance_type        = "t2.micro"
+    instance_type        = "t3.micro"
 }
 
 resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
